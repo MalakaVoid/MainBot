a5657109c8c381e01664a3aa9265dda2b92978f1
MalakaVoid <azbukin2003@gmail.com>
2023-11-03 23:34:00 +0300

    2 blocks made: Main menu, Support, About company

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -9,7 +9,16 @@ from keyboards import start_keyboards
 
 router = Router()
 
+
 class SupportStates(StatesGroup):
+    startSupport = State()
+    makeRequest = State()
+
+
+class AboutCompanyStates(StatesGroup):
+    start = State()
+    mainInfo = State()
+    services = State()
 
 
 @router.message(CommandStart())
@@ -17,15 +26,83 @@ async def start_command(message: Message) -> None:
     """
     START COMMAND HANDLER
     """
-    await message.answer("Начало бота",
+    await message.answer("Главное меню",
+                         reply_markup=start_keyboards.get_main_menu_kb())
+
+
+@router.message(F.text == "Главное меню")
+async def go_to_main_menu_btn(message: Message):
+    """
+    ОТПРАВКА В ГЛАВНОЕ МЕНЮ ИЗ ЛЮБОЙ ТОЧКИ
+    """
+    await message.answer("Главное меню",
                          reply_markup=start_keyboards.get_main_menu_kb())
 
 
 @router.message(F.text == "Поддержка")
-async def support_btn(message: Message) -> None:
+async def support_btn(message: Message, state: FSMContext) -> None:
     """
     ОБРАБОТЧИК КНОПКИ ПОДДЕРЖКИ
     """
+    await state.set_state(SupportStates.startSupport)
     await message.answer("Поддержка",
                          reply_markup=start_keyboards.get_support_menu_kb())
 
+
+@router.message(SupportStates.startSupport, F.text == "Назад")
+async def back_from_support_btn(message: Message, state: FSMContext) -> None:
+    """
+    ОБРАБОТЧИК КНОПКИ НАЗАД ИЗ ПОДДЕРЖКИ
+    """
+    await message.answer("Главное меню",
+                         reply_markup=start_keyboards.get_main_menu_kb())
+    await state.clear()
+
+
+@router.message(SupportStates.startSupport, F.text == "Назад")
+async def make_request_to_support_btn(message: Message, state: FSMContext) -> None:
+    """
+    ОБРАБОТЧИК КНОПКИ ОФОРМИТЬ ЗАЯВКУ В ПОДДЕРЖКУ
+    """
+    await message.answer("Оформить заявку",
+                         reply_markup=None)
+    await state.set_state(SupportStates.makeRequest)
+
+
+@router.message(F.text == "О компании")
+async def about_company_btn(message: Message, state: FSMContext):
+    """
+    ОБРАБОТЧИК КНОПКИ О КОМПАНИИ
+    """
+    await state.set_state(AboutCompanyStates.start)
+    await message.answer("О компании",
+                         reply_markup=start_keyboards.get_about_company_kb())
+
+
+@router.message(AboutCompanyStates.start, F.text == "Основная информация")
+async def main_info_about_company_btn(message: Message, state: FSMContext):
+    """
+    ОБРАБОТЧИК КНОПКИ ОСНОВАНЯ ИНФОРМАЦИЯ О КОМПАНИИ
+    """
+    await state.set_state(AboutCompanyStates.mainInfo)
+    await message.answer("Основная информация",
+                         reply_markup=start_keyboards.get_about_company_main_info())
+
+
+@router.message(AboutCompanyStates.mainInfo, F.text == "Привелегии")
+async def main_info_about_company_privelegies_btn(message: Message, state: FSMContext):
+    """
+    ОБРАБОТЧИК КНОПКИ ПРИВЕЛЕГИИ В ОСНОВНОЙ ИНФОРМАЦИИ О КОМПАНИИ
+    """
+    await message.answer("Привелегии",
+                         reply_markup=start_keyboards.get_only_main_menu_btn())
+
+
+@router.message(AboutCompanyStates.start, F.text == "Сервисы")
+async def about_company_services_btn(message: Message, state: FSMContext):
+    """
+    ОБРАБОТЧИК КНОПКИ СЕРВИСЫ ИЗ О КОМПАНИИ
+    """
+    await state.set_state(AboutCompanyStates.services)
+    await message.answer("Сервисы",
+                         reply_markup=start_keyboards.get_about_compony_services())
--- keyboards/start_keyboards.py
+++ keyboards/start_keyboards.py
@@ -3,7 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButt
 
 def get_main_menu_kb() -> ReplyKeyboardMarkup:
     """
-    Default main keyboard
+    ГЛАВНОЕ МЕНЮ
     """
     btn_1 = KeyboardButton(text="Активировать подписку")
     btn_2 = KeyboardButton(text="Поддержка")
@@ -17,7 +17,7 @@ def get_main_menu_kb() -> ReplyKeyboardMarkup:
 
 def get_support_menu_kb() -> ReplyKeyboardMarkup:
     """
-    Support keyboard
+    ПОДДЕРЖКА
     """
     btn_1 = KeyboardButton(text="Оформить заявку")
     btn_2 = KeyboardButton(text="Назад")
@@ -28,3 +28,64 @@ def get_support_menu_kb() -> ReplyKeyboardMarkup:
     )
     return kb
 
+
+def get_about_company_kb() -> ReplyKeyboardMarkup:
+    """
+    О КОМПАНИИ
+    """
+    btn_1 = KeyboardButton(text="Основная информация")
+    btn_2 = KeyboardButton(text="Сервисы")
+
+    kb = ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        keyboard=[[btn_1, btn_2]]
+    )
+    return kb
+
+
+def get_about_company_main_info() -> ReplyKeyboardMarkup:
+    """
+    О КОМПАНИИ - ОСНОВНАЯ ИНФОРМАЦИЯ -
+
+    --ПРИВЕЛЕГИИ--
+    """
+    btn_1 = KeyboardButton(text="Привелегии")
+    btn_2 = KeyboardButton(text="Главное меню")
+
+    kb = ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        keyboard=[[btn_1], [btn_2]]
+    )
+    return kb
+
+
+def get_about_compony_services() -> ReplyKeyboardMarkup:
+    """
+    О КОМПАНИИ - СЕРВИСЫ
+
+    --все сервисы--
+    """
+    btn_1 = KeyboardButton(text="S1")
+    btn_2 = KeyboardButton(text="S2")
+    btn_3 = KeyboardButton(text="S3")
+    btn_4 = KeyboardButton(text="Главное меню")
+
+    kb = ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        keyboard=[[btn_1],[btn_2],[btn_3],[btn_4]]
+    )
+    return kb
+
+
+def get_only_main_menu_btn() -> None:
+    """
+    ТОЛЬКО КНОПКА ВОЗВРАТА В ГЛАВНОЕ МЕНЮ
+    ENDPOINT
+    """
+    btn_1 = KeyboardButton(text="Главное меню")
+
+    kb = ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        keyboard=[[btn_1]]
+    )
+    return kb
